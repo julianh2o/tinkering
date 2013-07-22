69f0f27795c869e5967997e527e18b05a5c9a5f6
Julian Hartline <julianh2o@gmail.com>
2013-07-21 20:35:57 -0700

    about to tweak some assembly to try to improve timing

--- pic18f25k80.X/colors.py
+++ pic18f25k80.X/colors.py
@@ -2,11 +2,15 @@
 
 color = 0;
 numbers = [];
-while (color <= 120):
+while (color <= 124):
     r = 0
     g = 0
     b = 0
     if (color % 10 == 0): b = 10
+    if (color % 30 == 0): g = 10
+    r = 10
+    g = 10
+    b = 10
     numbers += [g,r,b]
     color += 1
 
