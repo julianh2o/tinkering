ad5a3c2e8c261a9617522879b17e22d49750097a
Julian Hartline <julianh2o@gmail.com>
2013-09-04 18:56:09 -0700

    about to trash stuff

--- pic18f25k80.X/nRF2401_config.h
+++ pic18f25k80.X/nRF2401_config.h
@@ -6,8 +6,8 @@
 #define TRIS_CE		TRISCbits.TRISC1
 #define CSN		PORTCbits.RC2
 #define TRIS_CSN	TRISCbits.TRISC2
-#define IRQ		PORTCbits.RC6
-#define TRIS_IRQ	TRISCbits.TRISC6
+#define IRQ		PORTCbits.RC7
+#define TRIS_IRQ	TRISCbits.TRISC7
 
 #define TRIS_SCK	TRISCbits.TRISC3
 #define TRIS_MISO	TRISCbits.TRISC4
