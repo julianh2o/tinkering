aa8e0148f494ba1e94cf84518b02f28a9578fb17
Julian Hartline <julianh2o@gmail.com>
2013-09-04 21:46:52 -0700

    got the wireless rainbow working

--- pic18f25k80.X/ledstripwireless.c
+++ pic18f25k80.X/ledstripwireless.c
@@ -20,13 +20,12 @@
 #define MODE_SELECT_TRIS TRISBbits.TRISB2
 #define MODE_SELECT PORTBbits.RB2
 
-//#define STRIP_LENGTH 125
-#define STRIP_LENGTH 5
+#define STRIP_LENGTH 125
 #define DATA_SIZE 375
 
 #pragma idata large_idata
 //char led_buffer[375] = {0,10,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,10,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,10,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,10,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,10,0,0,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,0,10,0,10,0,0,0,10,0,0,10,0,0,10,0,0,10};
-char led_buffer[375] = {10,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+char led_buffer[375] = {10,0,0,0,10,0,0,0,10,10,10,10,0,0,10,0,10,0,10,0,0,10,10,10,0,10,0,10,0,0,0,0,10,10,10,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 const char rainbow[375] = {0,15,0,0,15,0,1,15,0,2,15,0,3,15,0,3,15,0,4,15,0,5,15,0,6,15,0,6,15,0,7,15,0,8,15,0,9,15,0,9,15,0,10,15,0,11,15,0,12,15,0,13,15,0,13,15,0,14,15,0,15,15,0,15,15,0,15,15,0,15,14,0,15,13,0,15,12,0,15,11,0,15,11,0,15,10,0,15,9,0,15,8,0,15,8,0,15,7,0,15,6,0,15,5,0,15,5,0,15,4,0,15,3,0,15,2,0,15,2,0,15,1,0,15,0,0,15,0,0,15,0,1,15,0,1,15,0,2,15,0,3,15,0,4,15,0,4,15,0,5,15,0,6,15,0,7,15,0,7,15,0,8,15,0,9,15,0,10,15,0,10,15,0,11,15,0,12,15,0,13,15,0,14,15,0,14,15,0,15,15,0,15,14,0,15,14,0,15,13,0,15,12,0,15,11,0,15,10,0,15,10,0,15,9,0,15,8,0,15,7,0,15,7,0,15,6,0,15,5,0,15,4,0,15,4,0,15,3,0,15,2,0,15,1,0,15,1,0,15,0,0,15,0,0,15,0,1,15,0,2,15,0,2,15,0,3,15,0,4,15,0,5,15,0,5,15,0,6,15,0,7,15,0,8,15,0,8,15,0,9,15,0,10,15,0,11,15,0,11,15,0,12,15,0,13,15,0,14,15,0,15,15,0,15,15,0,15,15,0,15,14,0,15,13,0,15,13,0,15,12,0,15,11,0,15,10,0,15,9,0,15,9,0,15,8,0,15,7,0,15,6,0,15,6,0,15,5,0,15,4,0,15,3,0,15,3,0,15,2,0,15,1,0,15,0};
 #pragma idata
 
@@ -42,7 +41,7 @@ void sendStrip();
 void loadFrame(char frame);
 void updateBuffer();
 
-void doRainbow(void);
+void writeRainbowWithOffset(short offset);
 void delay(void);
 extern void updateLEDs(void);
 
@@ -104,13 +103,13 @@ void run(void) {
 
 void updateBuffer() {
     char loc = rx_buf[0];
-    char i;
+    short i;
 
     //0,1,2,3,4 = (status info)
     //0 = multiplier
     //5-29 = data
     for (i = 0; i<25; i++) {
-        led_buffer[25*loc+i] = rx_buf[5+i];
+        led_buffer[25*((short)loc)+i] = rx_buf[5+i];
     }
 }
 
@@ -133,9 +132,9 @@ void runStrip() {
         
         STATUS_LED = nrf_Recieve(&rx_buf);
 
-//        updateBuffer();
-//
-//        updateLEDs();
+        updateBuffer();
+        
+        updateLEDs();
         Delay10KTCYx(50);
     }
 }
@@ -148,17 +147,22 @@ int readPotentiometer() {
 }
 
 void loadFrame(char frame) {
-    char i;
+    short i;
+    tx_buf[0] = frame;
+    tx_buf[1] = 0;
+    tx_buf[2] = 0;
+    tx_buf[3] = 0;
+    tx_buf[4] = 0;
     for (i=0; i<25; i++) {
-        tx_buf[i] = led_buffer[frame*25+i];
+        tx_buf[5+i] = led_buffer[((int)frame)*25+i];
     }
 }
 
 void sendStrip() {
-    char i;
+    short i;
     for (i=0; i<15; i++) {
         loadFrame(i);
-        nrf_Send(&tx_buf, &rx_buf);
+        STATUS_LED = nrf_Send(&tx_buf, &rx_buf);
     }
 }
 
@@ -175,7 +179,6 @@ void displayStatus(char status) {
     runFlag = !runFlag;
     
     setPosition(1,0);
-    if (status & 0b10000000) sendLiteralBytes("RES ");
     if (status & 0b1000000) sendLiteralBytes("DR ");
     if (status & 0b100000) sendLiteralBytes("DS ");
     if (status & 0b10000) sendLiteralBytes("RT ");
@@ -184,19 +187,15 @@ void displayStatus(char status) {
 }
 
 void runControl() {
+    int fixweirdbehavior;
     unsigned char status;
-    unsigned char config;
-    char i;
+    short i;
     int value;
 
-    //OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_INT_OFF, 0b0000);
+    OpenADC(ADC_FOSC_64 & ADC_RIGHT_JUST & ADC_20_TAD, ADC_CH0 & ADC_INT_OFF, 0b0000);
     
     nrf_init();
     delay();
-
-//    config = SPI_Read(CONFIG);
-//    sendHex(config);
-//    sendLiteralBytes("  ");
 
     initTX();
     delay();
@@ -214,73 +213,47 @@ void runControl() {
 //        delay();
 //    }
     while(1) {
-        //STATUS_LED = !STATUS_LED;
-        //value = readPotentiometer();
-        //value = value >> 4;
-
-//        tx_buf[0] = 0;
-//        tx_buf[1] = 0;
-//        tx_buf[2] = 0;
-//        tx_buf[3] = 0;
-//        tx_buf[4] = 0;
-//
-//        for (i=5; i<30; i++) {
-//            tx_buf[i] = 5;
-//        }
-        setupLCD();
-        status = getStatus();
-
-//        config = SPI_Read(CONFIG);
-//        clear();
-//        sendLiteralBytes("config: ");
-//        sendHex(config);
-//        setPosition(1,0);
-//        sendBinPad(config);
-//        delay();
+//        setupLCD();
+//        status = getStatus();
+//        displayStatus(status);
         
-//        clear();
-//        //sendBinPad(config);
-//
-//        //sendHex(config);
-//        sendDec((unsigned char)150);
-//        sendLiteralBytes(" ");
-//        sendBin(config);
-//        sendLiteralBytes(" ");
-//        sendHex(config);
-//        delay();
-        displayStatus(status);
         //STATUS_LED = !STATUS_LED;
-        STATUS_LED = nrf_Send(&tx_buf, &rx_buf);
+        value = readPotentiometer();
+        value = value >> 4;
+//
+//        clear();
+//        sendIntDec(value);
+//
+        for (i=0; i<125; i++) {
+            led_buffer[i*3] = 5;
+            led_buffer[i*3+1] = 5;
+            led_buffer[i*3+2] = 5;
+        }
 
-        //for (i=0; i<10; i++) {
-            //led_buffer[i] = value;
-        //}
+//        led_buffer[0] = value;
+//        led_buffer[1] = value;
+//        led_buffer[2] = value;
 
-        //sendStrip();
-        Delay10KTCYx(50);
+        value = value >> 1;
+        writeRainbowWithOffset(value);
+
+        sendStrip();
     }
 
     //CloseADC();
 }
 
-void doRainbow(void) {
-    short i,offset,i_rainbow;
+void writeRainbowWithOffset(short offset) {
+    short i,i_rainbow;
 
-    offset = 0;
-    while(1) {
-        i_rainbow = offset;
-        for (i=0; i<STRIP_LENGTH; i++) {
-            led_buffer[i*3] = rainbow[i_rainbow*3];
-            led_buffer[i*3+1] = rainbow[i_rainbow*3+1];
-            led_buffer[i*3+2] = rainbow[i_rainbow*3+2];
+    i_rainbow = offset;
+    for (i=0; i<STRIP_LENGTH; i++) {
+        led_buffer[i*3] = rainbow[i_rainbow*3];
+        led_buffer[i*3+1] = rainbow[i_rainbow*3+1];
+        led_buffer[i*3+2] = rainbow[i_rainbow*3+2];
 
-            i_rainbow++;
-            if (i_rainbow >= STRIP_LENGTH) i_rainbow = 0;
-        }
-        updateLEDs();
-        Delay10KTCYx(50);
-        offset++;
-        if (offset >= STRIP_LENGTH) offset = 0;
+        i_rainbow++;
+        if (i_rainbow >= STRIP_LENGTH) i_rainbow = 0;
     }
 }
 
